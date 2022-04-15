200a8d3f63822ad542958812168d20ed8e00b4c5
Katya Grenier <katyagrenier@Katyas-MacBook-Air.local>
2022-04-15 15:14:37 +0100

    completed todos for first 3 sections

---
+++ src/main/java/org/example/day4/_3_strings/StringExercises.java
@@ -0,0 +1,96 @@
+package org.example.day4._3_strings;
+
+public class StringExercises {
+    /*
+       TODO 9
+       Prefix the param with 'Good job, '
+
+       goodJob("Anna")          -> "Good job, Anna!"
+       goodJob("my friend")     -> "Good job, my friend!"
+       goodJob("XYZ")           -> "Good job, XYZ!"
+       goodJob("Indiana Jones") -> "Good job, Indiana Jones!"
+
+    */
+    String goodJob(String name) {
+        return "Good job, " + name + "!";
+    }
+
+    /*
+       TODO 10
+       If String is even length, return first half x2. Else return original String.
+
+       doubleFirstHalf("HeyYou")   -> "HeyHey"
+       doubleFirstHalf("12345678") -> "12341234"
+       doubleFirstHalf("M")        -> "M"
+       doubleFirstHalf("panda")    -> "panda"
+    */
+    String doubleFirstHalf(String str) {
+
+        if(str.length() % 2 == 0){
+            return str.substring(0, str.length()/2) + str.substring(0, str.length()/2);
+        }else{
+            return str;
+        }
+    }
+
+    /*
+        TODO 11
+
+        interlace("A", "B")      -> "ABABA"
+        interlace("Sun", "Moon") -> "SunMoonSunMoonSun"
+        interlace("blue", "red") -> "blueredblueredblue"
+        interlace("Hi", "Ciao")  -> "HiCiaoHiCiaoHi"
+     */
+    String interlace(String a, String b) {
+
+        return a + b + a + b + a;
+    }
+
+    /*
+        TODO 12
+
+        cutEnds("Java")  -> "av"
+        cutEnds("koala") -> "oal"
+        cutEnds("XX")    -> ""
+        cutEnds("a")     -> ""
+     */
+    String cutEnds(String str) {
+
+        if(str.length() > 1){
+            return str.substring(1, str.length()-1);
+        }else{
+            return "";
+        }
+    }
+
+    /*
+
+       TODO 13
+
+       addTags("smile", "i")        -> <i>smile</i>
+       addTags("777666555", "phone" -> <phone>777666555</phone>
+       addTags("welcome", "header"  -> <header>welcome</header>
+     */
+    String addTags(String word, String tag) {
+
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    /*
+
+       TODO 14
+
+       copyLastChars("computer") -> "ter ter"
+       copyLastChars("ABCD")     -> "BCD BCD"
+       copyLastChars("Hi")       -> "Hi Hi"
+       copyLastChars("Amazing")  -> "ing ing"
+     */
+    String copyLastChars(String str) {
+
+        if(str.length() > 2){
+            return str.substring(str.length()-3) + " " + str.substring(str.length()-3) ;
+        } else {
+            return str + " " + str;
+        }
+    }
+}
